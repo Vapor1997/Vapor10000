3bbb760903c4c9bee4944e2a7e36c73f89524e57
Vapor1997 <893201500@qq.com>
2017-09-12 19:54:42 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/vapor10000/aiActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.vapor10000;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ai);
+    }
+}
